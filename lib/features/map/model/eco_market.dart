467c55604cd800fc8d3fd8fa41ec1d660878b977
migalha212 <migalha240@gmail.com>
2025-05-13 12:32:38 +0100

    refactor corrections

--- lib/features/map/model/eco_market.dart
+++ lib/features/map/model/eco_market.dart
@@ -5,9 +5,12 @@ class EcoMarket {
   final String id;
   final String name;
   final String? description;
+  final String? address;
+  final String? website;
   final double latitude;
   final double longitude;
   final DocumentReference reference;
+
 
   EcoMarket({
     required this.id,
@@ -16,6 +19,8 @@ class EcoMarket {
     required this.longitude,
     required this.reference,
     this.description,
+    this.address,
+    this.website,
   });
 
   LatLng get position => LatLng(latitude, longitude);
--- lib/features/map/presentation/widgets/marker_sheet.dart
+++ lib/features/map/presentation/widgets/marker_sheet.dart
@@ -1,16 +1,89 @@
 import 'package:eco_finder/features/map/model/eco_market.dart';
 import 'package:flutter/material.dart';
 import 'package:eco_finder/utils/navigation_items.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class StoreBottomSheet extends StatelessWidget {
   final EcoMarket business;
 
   const StoreBottomSheet({super.key, required this.business});
 
+  Widget _buildWebsiteRow(
+    IconData icon,
+    String label,
+    String? value,
+    BuildContext context,
+  ) {
+    if (value == null || value.isEmpty) return const SizedBox.shrink();
+    String? url = value;
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 8.0),
+      child: InkWell(
+        onTap: () async {
+          if (url != null) {
+            String processedUrl = url;
+            if (!processedUrl.startsWith('http://') &&
+                !processedUrl.startsWith('https://')) {
+              processedUrl = 'https://$processedUrl';
+            }
+            final Uri uri = Uri.parse(processedUrl);
+            try {
+              final bool launched = await launchUrl(
+                uri,
+                mode: LaunchMode.externalApplication,
+              );
+              if (!launched) {
+                ScaffoldMessenger.of(context).showSnackBar(
+                  const SnackBar(
+                    content: Text('Não foi possível abrir o link do website.'),
+                  ),
+                );
+              }
+            } catch (e) {
+              ScaffoldMessenger.of(context).showSnackBar(
+                const SnackBar(
+                  content: Text('Ocorreu um erro ao abrir o link.'),
+                ),
+              );
+            }
+          }
+        },
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Icon(icon, color: const Color(0xFF3E8E4D)),
+            const SizedBox(width: 12.0),
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    label,
+                    style: const TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black87,
+                    ),
+                  ),
+                  const SizedBox(height: 4.0),
+                  Text(
+                    value!,
+                    style: const TextStyle(fontSize: 14, color: Colors.black54),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return FractionallySizedBox(
       heightFactor: 0.45,
+      widthFactor: 1.0,
       child: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Column(
@@ -24,21 +97,61 @@ class StoreBottomSheet extends StatelessWidget {
                 borderRadius: BorderRadius.circular(10),
               ),
             ),
-            Text(
-              business.name,
-              style: const TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
-            ),
-            const SizedBox(height: 10),
-            Expanded(
-              child: SingleChildScrollView(
-                child: Text(
-                  business.description ?? '',
-                  textAlign: TextAlign.center,
-                  style: const TextStyle(fontSize: 16),
+            Center(
+              child: Text(
+                business.name,
+                style: const TextStyle(
+                  fontSize: 25,
+                  fontWeight: FontWeight.bold,
                 ),
               ),
             ),
+             if (business.description != null)
+              Text(
+                business.description!,
+                textAlign: TextAlign.left,
+                style: const TextStyle(fontSize: 16),
+                maxLines: 3, // Limitando a descrição a 3 linhas
+                overflow:
+                    TextOverflow
+                        .ellipsis, // Adicionando "..." para indicar texto oculto
+              ),
             const SizedBox(height: 10),
+            if (business.address != null)
+              GestureDetector(
+                onTap: () {
+                  final address = Uri.encodeComponent(business.address!);
+                  final url =
+                      'https://www.google.com/maps/search/?api=1&query=$address';
+                  launchUrl(Uri.parse(url));
+                },
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    const Icon(Icons.location_on, color: Color(0xFF3E8E4D)),
+                    const SizedBox(width: 8),
+                    Flexible(
+                      child: Text(
+                        business.address!,
+                        style: const TextStyle(
+                          fontSize: 16,
+                          color: Colors.black,
+                          decoration: TextDecoration.none,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            const SizedBox(height: 10),
+            if(business.website != null)
+              _buildWebsiteRow(
+                Icons.language_outlined,
+                'Website',
+                business.website,
+                context
+              ),
+            const SizedBox(height: 20),
             ElevatedButton.icon(
               onPressed: () {
                 Navigator.pushNamed(
